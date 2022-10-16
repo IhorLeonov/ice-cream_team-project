3175c0a88c4641ee6376ba887c89e7badf27a898
IhorLeonov <110539763+IhorLeonov@users.noreply.github.com>
2022-10-16 09:24:00 +0000

    Deploying to gh-pages from @ IhorLeonov/project_group_20@2c46198f0d558d3a97de9c136613541f6a5ae547 🚀

---
+++ index.3b8bebdb.js
@@ -0,0 +1,2 @@
+$(".gallery-slider").slick({dots:!1,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0,autoplay:!0,autoplaySpeed:2e3}),$(".reviews-slider").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0});
+//# sourceMappingURL=index.3b8bebdb.js.map
