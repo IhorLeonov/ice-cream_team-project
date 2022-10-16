746eb6af7ca8db0fcaa51bf31f16896b4e3883d8
Anatolij Goga <anzefal@gmail.com>
2022-10-16 09:51:09 +0300

    Created src\js\slick.js and add script to it

---
+++ src/js/slick.js
@@ -0,0 +1,17 @@
+$('.gallery-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+$('.reviews-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+});
