797768607ee22450818473d11e8e7062701d3d67
IhorLeonov <110539763+IhorLeonov@users.noreply.github.com>
2022-10-16 10:46:00 +0000

    Deploying to gh-pages from @ IhorLeonov/project_group_20@2816f1630fe3670da04dd4917b282b9e5b3d6985 🚀

---
+++ index.54eb73bb.js
@@ -0,0 +1,2 @@
+$(".gallery-slider").slick({dots:!1,infinite:!0,speed:200,slidesToShow:1,adaptiveHeight:!0,autoplay:!1,autoplaySpeed:2e3}),$(".reviews-slider").slick({dots:!0,infinite:!0,speed:200,slidesToShow:1,adaptiveHeight:!0});
+//# sourceMappingURL=index.54eb73bb.js.map
