2953d631a748e2baade6f42625827ad79119be69
smolianko2503 <smolianko.andrey1993@gmail.com>
2022-10-21 00:52:09 +0300

    add slow-scroll and close mobile-menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,17 +1,42 @@
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
+  $(document).ready(function () {
+  // Add smooth scrolling to all links
+  $('a').on('click', function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== '') {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    }
+    if (mobileMenu.classList[2] !== 'is-open') {
+      return;
+    }
+    mobileMenu.classList.toggle('is-open');
+  });
+});
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+   const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
+    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
   };
 
---
+++ src/js/slow-scroll.js
@@ -0,0 +1,21 @@
+
+$('a[href*="#contacts"]').on('click', function() {
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 1000);
+  return false;
+});
+
+$('a[href*="#products"]').on('click', function() {
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 1000);
+  return false;
+});
+
+$('a[href*="#about"]').on('click', function() {
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 1000);
+  return false;
+});
